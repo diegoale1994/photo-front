7a6796734b112b891d5dba6f78f9135cd46b7409
Diego Alejandro <diego.fc.1@hotmail.com>
2019-05-14 15:00:28 -0500

    reset password and home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,89 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AlertService } from 'src/app/services/alert.service';
+import { AccountService } from 'src/app/services/account.service';
+import { Router } from '@angular/router';
+import { LoadingService } from 'src/app/services/loading.service';
+import { Subscription } from 'rxjs';
+import { AlertType } from '../../enum/alert-type.enum';
+import { User } from 'src/app/model/user';
+import { Post } from 'src/app/model/post';
+import { PostService } from 'src/app/services/post.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
+export class HomeComponent implements OnInit, OnDestroy {
+  private subscriptions: Subscription[] = [];
+  user = new User();
+  posts: Post[] = [];
+  host: string;
+  userHost: string;
+  postHost: string;
+  constructor(private alertService: AlertService, private accountService: AccountService, private router: Router,
+              private loadingService: LoadingService, private postService: PostService) { }
 
   ngOnInit() {
+
+  }
+
+  getUserInfo(username: string): void {
+    this.subscriptions.push(
+      this.accountService.getUserInformation(username).subscribe((response: User) => {
+        this.user = response;
+      }, error => {
+        console.log(error);
+        this.user = null;
+        this.logOut();
+        this.router.navigateByUrl('/login');
+      })
+    );
+  }
+  logOut() {
+   this.accountService.logOut();
+   this.router.navigateByUrl('/login');
+   this.alertService.showAlert('Necesitas entrar a la aplicacion para ver esta pagina', AlertType.DANGER);
+  }
+
+  getUserProfile(username: string): void {
+    this.router.navigate(['/profile', username]);
+    console.log(username);
+  }
+
+  getPosts(): void {
+    this.subscriptions.push(
+      this.accountService.getPosts().subscribe((response: Post[]) => {
+        this.posts = response;
+        console.log(this.posts);
+        this.loadingService.loading.next(false);
+      }, error => {
+        console.log(error);
+        this.loadingService.loading.next(false);
+      })
+    );
+  }
+
+  onDelete(id: number) {
+    this.subscriptions.push(
+      this.postService.deletePost(id).subscribe(response => {
+        console.log(response);
+        this.alertService.showAlert('Post borrado', AlertType.SUCCESS);
+        this.getPosts();
+      }, error => {
+        console.log(error);
+        this.alertService.showAlert('ha ocurrido un error intentalo mas tarde', AlertType.DANGER);
+        this.getPosts();
+      })
+    );
+  }
+
+  seeOnePost(postId): void {
+    this.router.navigate(['/post', postId]);
+    console.log(postId);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
 }
--- src/app/components/reset-password/reset-password.component.ts
+++ src/app/components/reset-password/reset-password.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AlertService } from 'src/app/services/alert.service';
+import { AccountService } from 'src/app/services/account.service';
+import { Router } from '@angular/router';
+import { LoadingService } from 'src/app/services/loading.service';
+import { Subscription } from 'rxjs';
+import { AlertType } from '../../enum/alert-type.enum';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
   styleUrls: ['./reset-password.component.css']
 })
-export class ResetPasswordComponent implements OnInit {
-
-  constructor() { }
+export class ResetPasswordComponent implements OnInit, OnDestroy {
+  private subscriptions: Subscription[] = [];
+  constructor(private alertService: AlertService, private accountService: AccountService, private router: Router,
+              private loadingService: LoadingService) { }
 
   ngOnInit() {
+    if (this.accountService.isLoggedIn()) {
+      if (this.accountService.redirectURL) {
+        this.router.navigateByUrl(this.accountService.redirectURL);
+      } else {
+      this.router.navigateByUrl('/home');
+    }
+  } else {
+    this.router.navigateByUrl('/resetpassword');
+  }
+  }
+
+  onResetpassword(form) {
+    this.loadingService.loading.next(true);
+    console.log(form.email);
+    const email = form.email;
+    this.subscriptions.push(
+      this.accountService.resetPassword(email).subscribe(response => {
+        console.log(response);
+        this.loadingService.loading.next(false);
+        this.alertService.showAlert('Un nuevo password fue enviado al correo electronico', AlertType.SUCCESS);
+      }, (error: HttpErrorResponse) => {
+          console.log(error);
+          const errorMsg = error.error;
+          if (errorMsg === 'emailNotFound') {
+            this.alertService.showAlert('No existe una cuenta para ese correo', AlertType.DANGER);
+          }
+          if (errorMsg !== 'emailNotFound') {
+            this.alertService.showAlert('A ocurrido un error intentalo mas tarde', AlertType.DANGER);
+          }
+          this.loadingService.loading.next(false);
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+   this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
 }
